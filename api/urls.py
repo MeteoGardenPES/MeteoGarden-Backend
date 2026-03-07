b453aedbd2034910df120ec7aa20f89e480eaf95
mireiaa-16 <mireiarino@gmail.com>
2026-03-07 13:36:06 +0100

    #1 django configuration to build api of the backend

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+"""
+URL configuration for api.
+"""
+from django.contrib import admin
+from django.urls import path
+
+from .views import health
+
+urlpatterns = [
+    path('health/', health),
+]
