3322c1f16913bd3a2e1fbcb135433905c5ee6361
alexgomezz22 <alex.gomez.rodriguez@estudiantat.upc.edu>
2026-03-11 19:32:57 +0100

    feat: profile operations work

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,18 @@
 """
 URL configuration for api.
 """
-
-# from django.contrib import admin
 from django.urls import path
+from .views import edit_profile, get_profile, health, login, register
 
-from .views import health
+"""
+from django.contrib import admin
+"""
+
 
 urlpatterns = [
     path("health/", health),
+    path("register/", register),
+    path("login/", login),
+    path("get_profile/", get_profile),
+    path("edit_profile/", edit_profile),
 ]
