864f5661356812a4f39f451dccbb6a1ad87a78ac
laiacj <laia.cladera@estudiantat.upc.edu>
2026-03-12 21:03:13 +0100

    intento num 8 creo

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,4 +1,3 @@
-from .views import health  # noqa: F401
 from .views_visualitzarJardi import (garden_plants, plant_status, user_gardens,
                                      water_plant)
 from .views_xema import current_weather, get_stations  # noqa: F401
