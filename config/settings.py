2242b094c835d3725e5a24bc9685ba2be4822b52
mireiaa-16 <mireiarino@gmail.com>
2026-03-14 19:32:18 +0100

    Add Docker configuration

--- config/settings.py
+++ config/settings.py
@@ -112,8 +112,8 @@ REST_FRAMEWORK = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation."
-        "UserAttributeSimilarityValidator",
+        "NAME":
+            "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
