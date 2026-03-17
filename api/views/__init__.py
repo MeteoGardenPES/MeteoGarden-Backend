310b60b37eda610fc18bea459eb2f212dba99fe7
alexgomezz22 <alex.gomez.rodriguez@estudiantat.upc.edu>
2026-03-17 13:44:18 +0100

    change: add urls of view_profile_operations

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,2 +1,3 @@
 from .views import health  # noqa: F401
 from .views_xema import current_weather, get_stations  # noqa: F401
+from .views_profile_operations import register, login, get_profile, edit_profile
