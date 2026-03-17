64e3e55861a408e2277f239570b935d129bf5f3b
laiacj <laia.cladera@estudiantat.upc.edu>
2026-03-17 13:41:08 +0100

    pasa cicd?

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,3 +1,7 @@
-from .views_visualitzarJardi import (garden_plants, plant_status, user_gardens,
-                                     water_plant)
+from .views_visualitzarJardi import (
+    garden_plants,
+    plant_status,
+    user_gardens,
+    water_plant,
+)
 from .views_xema import current_weather, get_stations  # noqa: F401
