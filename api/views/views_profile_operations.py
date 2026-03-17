7fdb0f4dc1da3ea6aa8cb4daeffc68463505308b
alexgomezz22 <alex.gomez.rodriguez@estudiantat.upc.edu>
2026-03-17 12:49:26 +0100

    changes to pull request

--- api/views/views_profile_operations.py
+++ api/views/views_profile_operations.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from .models import User
+from ..models import User
 
 
 # Create your views here.
