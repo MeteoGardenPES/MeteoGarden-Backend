e51eb6e71375b30752a6ab4b640ff02005a6bcdb
laiacj <laia.cladera@estudiantat.upc.edu>
2026-03-17 17:54:47 +0100

    plantar planta

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -2,6 +2,7 @@ from .views import health  # noqa: F401
 from .views_identify import identifyPlant  # noqa: F401
 from .views_image import getUserAlbum  # noqa: F401
 from .views_info import importPlant  # noqa: F401
+from .views_plantar_planta import plant_seed  # noqa: F401
 from .views_profile_operations import (  # noqa: F401
     edit_profile,
     get_profile,
