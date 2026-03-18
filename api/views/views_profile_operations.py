088cc8e85829e7b1d2c315222bf417ba6c736366
alexgomezz22 <alex.gomez.rodriguez@estudiantat.upc.edu>
2026-03-18 17:34:11 +0100

    feat: register now creates Garden (requires a gardenName)

--- api/views/views_profile_operations.py
+++ api/views/views_profile_operations.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from ..models import Inventory, User
+from ..models import Garden, Inventory, User
 
 
 # Create your views here.
@@ -18,6 +18,12 @@ def health(request):
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def register(request):
+
+    # Check if the garden name isn't empty
+    garden_name = request.data.get("gardenName")
+    if not garden_name:
+        return Response({"error": "Garden name is mandatory"}, status=400)
+
     user = User.objects.create_user(
         username=request.data["username"],
         password=request.data["password"],
@@ -25,14 +31,19 @@ def register(request):
         city=request.data["city"],
         language=request.data["language"],
         numPlantsCollected=0,
-        codiEstacio=request.data["codiEstacio"],
+        stationCode=request.data["stationCode"],
     )
 
-    # We create the inventory
+    # Create the inventory
     Inventory.objects.create(user=user)
 
+    # Create the garden
+    Garden.objects.create(user=user, name=garden_name)
+
     token, created = Token.objects.get_or_create(user=user)
-    return Response({"token": token.key, "message": "User created"})
+    return Response(
+        {"token": token.key, "message": "User, garden and inventory created"}
+    )
 
 
 # Login view
@@ -47,7 +58,7 @@ def login(request):
         return Response(
             {"token": token.key, "username": user.username, "message": "Login correcte"}
         )
-    return Response({"error": "Credencials incorrectes"}, status=400)
+    return Response({"error": "Wrong credentials"}, status=400)
 
 
 # View profile
@@ -61,7 +72,7 @@ def get_profile(request):
             "username": user.username,
             "email": user.email,
             "city": user.city,
-            "codiEstacio": user.codiEstacio,
+            "stationCode": user.stationCode,
             "language": user.language,
             "lastEntry": user.lastEntry,
             "numPlantsCollected": user.numPlantsCollected,
@@ -80,11 +91,11 @@ def edit_profile(request):
     user.city = data.get("city", user.city)
     user.language = data.get("language", user.language)
     user.numPlantsCollected = data.get("numPlantsCollected", user.numPlantsCollected)
-    user.codiEstacio = data.get("codiEstacio", user.codiEstacio)
+    user.stationCode = data.get("stationCode", user.stationCode)
     if "password" in data:
         user.set_password(data["password"])
     try:
         user.save()
-        return Response({"message": "Perfil actualitzat"})
+        return Response({"message": "Actualized profile"})
     except Exception as e:
         return Response({"error": str(e)}, status=400)
