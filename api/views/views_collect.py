01963ce1d9c6b1ccb81e653a04612c62a8e8ac43
xiaoyao.qiu <xiaoyao.qiu@estudiantat.upc.edu>
2026-03-19 14:01:51 +0100

    afegir permission AllowAny

--- api/views/views_collect.py
+++ api/views/views_collect.py
@@ -1,12 +1,14 @@
 import random
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from api.models import Garden, GrowthState, Inventory, Plant, PlantInGarden, Pot, User
 
 
 @api_view(["POST"])
+@permission_classes([AllowAny])
 def collect_plant(request):
 
     username = request.data.get("username")
